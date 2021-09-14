fa1c23575764b0dd5f206bab9942fe3939988dc5
MidoriGuerrero <midori031096@gmail.com>
2021-09-14 16:26:56 -0500

    Actividad 04 FrontEnd

---
+++ src/app/components/add-medicamento/add-medicamento.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Medicamento } from 'src/app/models/medicamento.model';
+import { MedicamentoService } from 'src/app/services/medicamento.service';
+
+@Component({
+  selector: 'app-add-medicamento',
+  templateUrl: './add-medicamento.component.html',
+  styleUrls: ['./add-medicamento.component.css']
+})
+export class AddMedicamentoComponent implements OnInit {
+
+  constructor(private medicamentoService: MedicamentoService) { }
+
+  medicamento: Medicamento = {
+    idMedicamento: 0,  
+    nombre:'', 
+    precio: 0,
+    stock: 0,
+    laboratorio: '',  
+   };
+
+  ngOnInit(): void {
+  }
+
+  saveMedicamento() : void{
+    console.log(">>>Save Inicio Impresion");
+
+    console.log(this.medicamento);
+
+    console.log(">>>Save Fin Impresion");
+
+    this.medicamentoService.create(this.medicamento).subscribe(
+      response => {
+        console.log(response.mensaje);
+        alert(response.mensaje)
+      },
+      error =>{
+        console.log(error);
+      }
+    );
+  }
+
+}
---
+++ src/app/services/medicamento.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Medicamento } from 'src/app/models/medicamento.model';
+
+const baseUrl = 'http://localhost:8090/rest/medicamento';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class MedicamentoService {
+
+  constructor(private http: HttpClient) { 
+  }
+  
+  create (data:any): Observable <any>{
+    return this.http.post(baseUrl, data);
+
+  }
+
+
+}
